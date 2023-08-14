3d10790a7f7ab18325f4d25a293bd18e01f0d00f
animpsix639 <49347226+animpsix639@users.noreply.github.com>
2023-08-14 18:02:17 +0200

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 from yandex_music import Client
 import pickle
+from colorama import Fore
 from tqdm import tqdm
 import os
 
@@ -20,12 +21,18 @@ def get_yandex(token):
         pickle.dump(data, f)
 
 
-if __name__ == '__main__':
+def main_yandex():
     token = input('Your Yandex OAuth token (LEAVE BLANK IF TOKEN IS AVAILABLE): ')
-    i = 0 if input('Load all tracks again? [Y/n]') == 'y'.lower() else pickle.load(open('yandex_token', 'rb'))[1]
+    i = 0 if input('Load all tracks again? [Y/n]') == 'y'.lower() or os.stat('yandex_token').st_size == 0 else pickle.load(open('yandex_token', 'rb'))[1]
     if not token:
-        token = pickle.load(open('yandex_token', 'rb'))[0]
+        try:
+            token = pickle.load(open('yandex_token', 'rb'))[0]
+        except EOFError:
+            raise SystemExit(Fore.RED + 'Yandex OAuth token is necessary')
     else:
         pickle.dump([token, i], open('yandex_token', 'wb'))
-    
-    get_yandex(token)
+    get_yandex()
+
+
+if __name__ == '__main__':
+    main_yandex()
